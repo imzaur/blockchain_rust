09be5884517baa618e8182a0d1d8cc77cbb9f3ff
Zaur <imzaur777@gmail.com>
2022-02-04 12:26:31 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use blockchainlib::*;
+
+fn main() {
+    let difficulty = 0x00ffffffffffffffffffffffffffff;
+    let mut block = Block::new(0, 0, vec![0;32], 0, "Genesis block".to_owned(),
+    difficulty);
+
+    block.mine();
+    println!("{}", &block);
+
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+
+    for i in 1..=10 {
+        let mut block = Block::new(i, 0, last_hash, 0, "Another block".to_owned(),
+        difficulty);
+
+        block.mine();
+        println!("{}", &block);
+        last_hash = block.hash.clone();
+        blockchain.blocks.push(block);
+    }
+
+}
